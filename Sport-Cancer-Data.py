c3be68cf34ba09170817cfa7abe49a7aa542e174
Amin Zayeromali <amin.zayeromali@gmail.com>
2023-11-02 20:52:38 +0330

    Add new Codes ...

--- Sport-Cancer-Data.py
+++ Sport-Cancer-Data.py
@@ -72,3 +72,31 @@ plt.ylabel('v2pk (Oxygen Peak Intensity)')
 
 plt.xticks([2, 6], ['Healthy', 'Cancer Patients'])
 plt.show()
+
+import pandas as pd
+
+data = {
+    'Category': ['Healthy', 'Healthy', 'Healthy', 'Healthy', 'Healthy', 'Cancer Patients', 'Cancer Patients', 'Cancer Patients', 'Cancer Patients', 'Cancer Patients'],
+    'Exercise Intensity': ['0.1', '0.5', '1.0', '0.5', '0.1', '0.1', '1.0', '0.5', '1.0', '0.1'],
+    'v2pk': [63, 68, 73, 65, 60, 55, 75, 68, 80, 58]
+}
+
+df = pd.DataFrame(data)
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# Convert categorical variables to numeric
+df['Category'] = df['Category'].astype('category')
+df['Exercise Intensity'] = df['Exercise Intensity'].astype('category')
+
+from sklearn.preprocessing import LabelEncoder
+
+label_encoder = LabelEncoder()
+df['Category'] = label_encoder.fit_transform(df['Category'])
+df['Exercise Intensity'] = label_encoder.fit_transform(df['Exercise Intensity'])
+
+# حالا می‌توانید نمودار کرلیشن را بسازید
+plt.figure(figsize=(8, 6))
+sns.heatmap(df.corr(), annot=True, cmap='coolwarm', linewidths=0.5)
+plt.title('Correlation Heatmap')
+plt.show()
